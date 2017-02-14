b3caefbf0d29904bd6e7ad1a7fb4a5bc8c67b9c2
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-02-15 01:09:06 +1100

    Fix tasks sorting logic, and add updated_at sorting

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,7 +3,6 @@ class Task < ApplicationRecord
   def self.todos
     Task.all
       .reject { |t| t.done }
-      .sort { |a,b| b.priority <=> a.priority }
-      .sort { |a,b| a.skip_count <=> b.skip_count }
+      .sort { |a,b| [b.priority, a.skip_count, b.updated_at] <=> [a.priority, b.skip_count, a.updated_at] }
   end
 end
