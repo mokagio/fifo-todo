f2914042a1ba4defbb16fad9978b937d8eb7d1d6
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-02-15 00:19:51 +1100

    Add migration to set Task skip_count default to 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214130633) do
+ActiveRecord::Schema.define(version: 20170214131801) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "title"
     t.integer  "priority",   default: 1
     t.boolean  "done",       default: false
-    t.integer  "skip_count"
+    t.integer  "skip_count", default: 0
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
   end
