f336fc0d929a24030facc133da9db7479b12ca84
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-02-15 00:35:59 +1100

    Add method to fetch sorted tasks that have not been completed

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,9 @@
 class Task < ApplicationRecord
+
+  def self.todos
+    Task.all
+      .reject { |t| t.done }
+      .sort { |a,b| b.priority <=> a.priority }
+      .sort { |a,b| a.skip_count <=> b.skip_count }
+  end
 end
