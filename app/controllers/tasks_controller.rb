953272df7ca25f81edb01a00bfedba852a1887df
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-02-15 01:03:14 +1100

    Add create action for new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,4 +3,19 @@ class TasksController < ApplicationController
     @task = Task.new
     @tasks = Task.todos
   end
+
+  def create
+    task = Task.new(task_params)
+    unless task.save
+      flash[:error] = "Could not create the new task"
+    end
+
+    redirect_to '#index'
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :priority)
+  end
 end
